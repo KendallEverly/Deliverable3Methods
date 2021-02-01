fbd643b0a067b1570cc997443bf31c2cf9f3abb1
Kendall Everly <ccrawford2@usf.edu>
2021-01-31 20:46:32 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*Author: Kendall Everly
+  Date: 01/31/2021
+  Comments: This C# console application demonstrates the use of methods after getting input from a user.
+ */
+using System;
+
+namespace Deliverable3Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            //read user input
+            string input = Console.ReadLine();
+            //show message
+            Console.WriteLine("Hello " + input);
+            Console.ReadKey(true);
+        }
+        //end of main
+    }
+    //end of class
+}
+//end of method
